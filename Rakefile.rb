668bb2009f36326ffed7e5d93dd223ef640e3381
Mark Smith <mark.smith@rankinteractive.com>
2013-05-28 13:52:49 +0100

    Annotate Rakefile

--- Rakefile.rb
+++ Rakefile.rb
@@ -4,6 +4,10 @@ require 'fileutils'
 directory 'daily-mail-blocker'
 CLOBBER << 'daily-mail-blocker'
 
+
+# Building Tasks --------------------------------------------------------------
+
+# Copy static sources from src to dest:
 STATIC_SOURCES = FileList['src/*.json',
         'src/*.png', 'src/*.html', 'src/*.js']
 STATIC_SOURCES.each do |source|
@@ -14,6 +18,7 @@ STATIC_SOURCES.each do |source|
     task :build => target
 end
 
+# Compile Coffee files inside src to dest
 COFFEE_SOURCES = FileList['src/*.coffee']
 COFFEE_SOURCES.each do |source|
     target = source.pathmap('daily-mail-blocker/%n.js')
@@ -23,24 +28,39 @@ COFFEE_SOURCES.each do |source|
     task :build => target
 end
 
+# Copy the test directory to dest:
 task :copy_tests do
     FileUtils.cp_r 'src/test', 'daily-mail-blocker', :verbose => true
 end
 
-desc "Build the extension."
-task :build => :copy_tests
+
+# Packaging Tasks -------------------------------------------------------------
 
 file 'daily-mail-blocker.zip' => [:build, 'daily-mail-blocker'] + FileList['daily-mail-blocker/**/*.*'] do
     sh "zip -r daily-mail-blocker daily-mail-blocker"
 end
 CLOBBER << 'daily-mail-blocker.zip'
 
+
 file 'daily-mail-blocker.crx' => [:build, 'daily-mail-blocker'] + FileList['daily-mail-blocker/**/*.*'] do
     sh "chrome --pack-extension=#{File.expand_path('daily-mail-blocker')} --pack-extension-key=#{File.expand_path('daily-mail-blocker.pem')} --no-message-box"
 end
 CLOBBER << 'daily-mail-blocker.crx'
 
-desc "Create a zip file containing the extension - suitable for upload."
-task :package => ['daily-mail-blocker.zip', 'daily-mail-blocker.crx']
+#------------------------------------------------------------------------------
+# Top-Level Tasks
+#
+
+desc "Build the extension."
+task :build => :copy_tests
+
+desc "Build a zip file suitable for upload to the Google Play Store"
+task :zip => 'daily-mail-blocker.zip'
+
+desc "Build a crx file suitable for self-hosting"
+task :crx => 'daily-mail-blocker.crx'
+
+desc "Create distribution packages."
+task :package => [:zip, :crx]
 
 task :default => :build
